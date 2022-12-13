68f83155b10aed06f6907a813cb8081f95c550af
Kylie <kylieavie@gmail.com>
2022-12-13 16:01:46 +0800

    arrays qns done

---
+++ questions/arrays/selectionSort/index.js
@@ -0,0 +1,31 @@
+/** selection Sort **
+ *
+ * Sort the 'nums' array using selection sort.
+ *
+ * @example
+ * selectionSort([3, 2, 1]) -> [1, 2, 3]
+ * selectionSort([0, 12, 8]) -> [0, 8, 12]
+ * selectionSort([3, 4, 0, 2]) -> [0, 2, 3, 4]
+ *
+ */
+
+const selectionSort = (nums) => {
+  for (let i = 0; i < nums.length; i++) {
+    let currMin = i;
+    // find min elem in unsorted array
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[j] < nums[currMin]) {
+        currMin = j;
+      }
+    }
+    // swap found min elem with 1st elem
+    if (currMin !== i) {
+      let temp = nums[i];
+      nums[i] = nums[currMin];
+      nums[currMin] = temp;
+    }
+  }
+  return nums;
+};
+
+module.exports = selectionSort;
