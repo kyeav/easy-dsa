8b437c55366d28a35c31ed23f6073938bdcfff1f
Kylie <kylieavie@gmail.com>
2022-12-15 18:34:32 +0800

    linked lists palindrome done

--- questions/linkedLists/linkedListPalindrome/index.js
+++ questions/linkedLists/linkedListPalindrome/index.js
@@ -11,7 +11,26 @@
  */
 
 const isPalindrome = (head) => {
+  const values = [];
+  let curr = head;
 
+  while (curr) {
+    values.push(curr.val);
+    curr = curr.next;
+  }
+
+  let l = 0;
+  let r = values.length - 1;
+
+  while (l <= r) {
+    if (values[l] !== values[r]) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+
+  return true;
 };
 
 module.exports = isPalindrome;
