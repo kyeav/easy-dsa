443216b7265c3184fc01a33c26148bf6fa83ffbe
Kylie <kylieavie@gmail.com>
2022-12-15 18:16:40 +0800

    length of linked list done

--- questions/linkedLists/lengthOfLinkedList/index.js
+++ questions/linkedLists/lengthOfLinkedList/index.js
@@ -1,18 +1,22 @@
 /** Length of Linked List **
- * 
- * Find the length of the given linked list. 
- *  
+ *
+ * Find the length of the given linked list.
+ *
  * @example
  * lengthOfList(1 -> 2 -> 3) -> 3
  * lengthOfList(10 -> 20) -> 2
  * lengthOfList(100 -> 200 -> 300 -> 400) -> 4
- * 
+ *
  */
 
- const lengthOfList = (head) => {
-
-
-}
-
+const lengthOfList = (head) => {
+  let length = 0;
+  let curr = head;
+  while (curr) {
+    length++;
+    curr = curr.next;
+  }
+  return length;
+};
 
 module.exports = lengthOfList;
