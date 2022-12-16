cbe7bde845fb043895345b04c7a1c819d1ead5f2
Kylie <kylieavie@gmail.com>
2022-12-16 15:36:40 +0800

    middle of linked list done

--- questions/linkedLists/middleOfLinkedList/index.js
+++ questions/linkedLists/middleOfLinkedList/index.js
@@ -1,32 +1,31 @@
 /** Middle of Linked List **
- * 
- * Find the middle node of the given linked list. 
- *  
+ *
+ * Find the middle node of the given linked list.
+ *
  * @example
  * middle(1 -> 2 -> 3) -> 2
- * middle(10 -> 20 -> 30 -> 40) -> 30 
- * middle(100 -> 200 -> 300 -> 400 -> 500) -> 300 
- * 
+ * middle(10 -> 20 -> 30 -> 40) -> 30
+ * middle(100 -> 200 -> 300 -> 400 -> 500) -> 300
+ *
  */
 
- const middle = (head) => {
+const middle = (head) => {
+  let slow = head;
+  let fast = head;
 
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
 
-}
-
-
-
-
-
-
-
-
+  return slow;
+};
 
 //DO NOT EDIT BELOW THIS LINE
-class Node{
-  constructor (val, next){
-    this.val = val
-    this.next = next
+class Node {
+  constructor(val, next) {
+    this.val = val;
+    this.next = next;
   }
 }
 
